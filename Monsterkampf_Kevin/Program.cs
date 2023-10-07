51a7a149628d35594e69b31d87884b061495275e
TheLurch27 <144247368+TheLurch27@users.noreply.github.com>
2023-10-07 22:06:01 +0200

    init

---
+++ Monsterkampf_Kevin/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Monsterkampf_Kevin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
