8bad987915799ab9f15046b3c254485f5f7a11c4
TheLurch27 <kevinnituom1996.sae@t-online.de>
2024-01-28 13:58:45 +0100

    ICH HABE FERTIG

---
+++ Monster.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monsterkampf_Kevin
+{
+    class Monster
+    {
+        public float HP { get; set; }
+        public float AP { get; set; }
+        public float DP { get; set; }
+        public float S { get; set; }
+        public string Race { get; set; }
+
+        public Monster(float hP, float aP, float dP, float s, string race)
+        {
+            HP = hP;
+            AP = aP;
+            DP = dP;
+            S = s;
+            Race = race;
+        }
+
+        public void Attack(Monster target)
+        {
+            float damage = AP - target.DP;
+            damage = Math.Max(damage, 0);
+
+            target.HP -= damage;
+            Console.WriteLine($"{Race} attacks and deals {damage} damage to {target.Race}.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,37 +2,12 @@
 
 namespace Monsterkampf_Kevin
 {
-    class Monster
-    {
-        public float HP { get; set; }
-        public float AP { get; set; }
-        public float DP { get; set; }
-        public float S { get; set; }
-        public string Race { get; set; }
-
-        public Monster(float hP, float aP, float dP, float s, string race)
-        {
-            HP = hP;
-            AP = aP;
-            DP = dP;
-            S = s;
-            Race = race;
-        }
-
-        public void attack(Monster target)
-        {
-            float damage = AP - target.DP;
-            damage = Math.Max(damage, 0);
-
-            target.HP -= damage;
-            Console.WriteLine($"{Race} attacks and deals {damage} damage to {target.Race}.");
-        }
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose 2 Character for the fight: ");
+            Console.WriteLine("Choose 2 Characters for the fight: ");
+            
             Console.WriteLine("1 - Troll");
             Console.WriteLine("2 - Wizard");
             Console.WriteLine("3 - Goblin");
@@ -40,18 +15,24 @@ namespace Monsterkampf_Kevin
             int choice1, choice2;
             do
             {
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.Write("Choose Character 1: ");
                 choice1 = int.Parse(Console.ReadLine());
 
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.Write("Choose Character 2: ");
                 choice2 = int.Parse(Console.ReadLine());
 
                 if (choice1 == choice2)
                 {
                     Console.WriteLine("The monsters cannot be the same race. Please select again.");
+                    Console.ReadKey();
+                    Console.Clear();
+
+
                 }
             } while (choice1 == choice2);
-
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Enter the attributes for Monster 1 (Life points (HP), Attack strength (AP), Defense points (DP), Speed (S)): ");
             Console.Write("HP = ");
             float hp1 = float.Parse(Console.ReadLine());
@@ -62,6 +43,7 @@ namespace Monsterkampf_Kevin
             Console.Write("S = ");
             float s1 = float.Parse(Console.ReadLine());
 
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Enter the attributes for Monster 2 (Life points (HP), Attack strength (AP), Defense points (DP), Speed (S)): ");
             Console.Write("HP = ");
             float hp2 = float.Parse(Console.ReadLine());
@@ -71,10 +53,12 @@ namespace Monsterkampf_Kevin
             float dp2 = float.Parse(Console.ReadLine());
             Console.Write("S = ");
             float s2 = float.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.White;
 
             Monster monster1 = null;
             Monster monster2 = null;
 
+            // Das erste arme Schwein ähh Monster wird erstellt.
             switch (choice1)
             {
                 case 1:
@@ -88,6 +72,7 @@ namespace Monsterkampf_Kevin
                     break;
             }
 
+            // Hier wird das zweite Monster für den Kampf erstellt. (Macht ja sonst auch keinen Sinn) ^^
             switch (choice2)
             {
                 case 1:
@@ -108,18 +93,18 @@ namespace Monsterkampf_Kevin
 
                 if (monster1.S >= monster2.S)
                 {
-                    monster1.attack(monster2);
+                    monster1.Attack(monster2);
                     if (monster2.HP > 0)
                     {
-                        monster2.attack(monster1);
+                        monster2.Attack(monster1);
                     }
                 }
                 else
                 {
-                    monster2.attack(monster1);
+                    monster2.Attack(monster1);
                     if (monster1.HP > 0)
                     {
-                        monster1.attack(monster2);
+                        monster1.Attack(monster2);
                     }
                 }
 
@@ -127,10 +112,12 @@ namespace Monsterkampf_Kevin
             }
             if (monster1.HP <= 0)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine($"{monster2.Race} wins the fight in {round - 1} Rounds!");
             }
             else
             {
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine($"{monster1.Race} wins the fight in {round - 1} Rounds!");
             }
         }
